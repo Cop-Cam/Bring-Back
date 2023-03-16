e8d5d293fda676583b70780f280bb1e99b9c6fa4
PJacketG <anantamiyoruwijaya2@gmail.com>
2023-03-17 00:21:22 +0700

    Minor Update

--- WPG IV/Assets/Scripts/Game Manager/QuestManager.cs
+++ WPG IV/Assets/Scripts/Game Manager/QuestManager.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestManager : MonoBehaviour
+public class QuestManager : GenericSingletonClass<QuestManager>
 {
+    public ScriptableObject CurrentQuest;
+    private string CurrentQuestName;
+    private string CurrentQuestDescription;
+    private int CurrentQuestTimeLimit;
+    //private int CurrentQuestTimeLimit;
+
+    //objective, reward, nama quest, deskripsi, fail consequences, time limit
     // Start is called before the first frame update
     void Start()
     {
