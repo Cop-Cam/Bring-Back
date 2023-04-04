b398d2d5f8a720b1521d296a3fe6339bf44eb238
Rasvet <95231477+FajarYiris@users.noreply.github.com>
2023-04-04 14:35:28 +0700

    Sun
    - Added sun
    - Not yet integrated by time in game
    - WIP

--- WPG IV/Assets/Scripts/Game Manager/Lighting/LightingManager.cs
+++ WPG IV/Assets/Scripts/Game Manager/Lighting/LightingManager.cs
@@ -3,75 +3,17 @@ using UnityEngine;
 //[ExecuteAlways]
 public class LightingManager : GenericSingletonClass<LightingManager>
 {
-    //Scene References
-    //[SerializeField] private float timescale;
+    [SerializeField]Light sun;
+    [SerializeField]float speed = 10f;
+    private float currentRotation = 0.0f;
 
-    [SerializeField] private Light DirectionalLight;
-    [SerializeField] private LightingPreset Preset;
-    //Variables
-    //[SerializeField, Range(0, 24)] private float TimeOfDay;
+    [SerializeField] TimeManager time;
+    
+    void Update() {
+        //transform.Rotate(Time.deltaTime*time.hours*speed,0,0);
 
-
-    private void Update()
-    {
-        if (Preset == null)
-            return;
-
-        UpdateLighting(TimeManager.Instance.hours);
-
-        // if (Application.isPlaying)
-        // {
-        //     //(Replace with a reference to the game time)
-        //     //TimeOfDay += Time.deltaTime * timescale;
-        //     //TimeOfDay %= 24; //Modulus to ensure always between 0-24
-        //     UpdateLighting(TimeManager.instance.TimeOfDay / 24f);
-        // }
-        // else
-        // {
-        //     UpdateLighting(TimeManager.instance.TimeOfDay / 24f);
-        // }
-    }
-
-
-    private void UpdateLighting(float timePercent)
-    {
-        //Set ambient and fog
-        RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
-        RenderSettings.fogColor = Preset.FogColor.Evaluate(timePercent);
-
-        //If the directional light is set then rotate and set it's color, I actually rarely use the rotation because it casts tall shadows unless you clamp the value
-        if (DirectionalLight != null)
-        {
-            DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
-
-            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
-        }
-
-    }
-
-    //Try to find a directional light to use if we haven't set one
-    private void OnValidate()
-    {
-        if (DirectionalLight != null)
-            return;
-
-        //Search for lighting tab sun
-        if (RenderSettings.sun != null)
-        {
-            DirectionalLight = RenderSettings.sun;
-        }
-        //Search scene for light that fits criteria (directional)
-        else
-        {
-            Light[] lights = GameObject.FindObjectsOfType<Light>();
-            foreach (Light light in lights)
-            {
-                if (light.type == LightType.Directional)
-                {
-                    DirectionalLight = light;
-                    return;
-                }
-            }
-        }
+        float rotationAmount = speed * Time.deltaTime / 60.0f;
+        currentRotation += rotationAmount;
+        transform.rotation = Quaternion.Euler(0.0f, currentRotation, 0.0f);
     }
 }
--- WPG IV/Assets/Scripts/Game Manager/TimeManager.cs
+++ WPG IV/Assets/Scripts/Game Manager/TimeManager.cs
@@ -101,4 +101,6 @@ public class TimeManager : GenericSingletonClass<TimeManager>
     {
         return hours;
     }
+
+    
 }
