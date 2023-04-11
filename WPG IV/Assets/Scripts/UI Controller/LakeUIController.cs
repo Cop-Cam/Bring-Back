14e786d2ebc76b1fb1e35085afba69ca4b646e24
PJacketG <anantamiyoruwijaya2@gmail.com>
2023-04-11 20:58:46 +0700

    Minor Update To Lake UI
    - Update and fixes bug to Lake UI

--- WPG IV/Assets/Scripts/UI Controller/LakeUIController.cs
+++ WPG IV/Assets/Scripts/UI Controller/LakeUIController.cs
@@ -3,18 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Linq;
 
 public class LakeUIController : GenericSingletonClass<LakeUIController>
 {
     private LakeInventory lakeInventory;
-    
-    [Header("GameObject For UI")]
-    [SerializeField] private GameObject LakeInventoryUI;
-    
+    [SerializeField] private GameObject LakeInventoryCanvas;
     [SerializeField] private TextMeshProUGUI CurrentPlayerMoneyText;
-
     [SerializeField] private Image CurrentFishImages;
-
+    [SerializeField] private TextMeshProUGUI CurrentFishName;
+    [SerializeField] private GameObject CollectButton;
+    [SerializeField] private GameObject SellButton;
 
     // [Header("Insert A Prefab for Collect Button and The Grid Which The Button Will Be Instantiated")]
     // [SerializeField] private GameObject CollectButtonPrefab;
@@ -24,17 +23,16 @@ public class LakeUIController : GenericSingletonClass<LakeUIController>
     // [SerializeField] private GameObject SellButtonPrefab;
     // [SerializeField] private GameObject SellButtonGridLayout;
 
-    [SerializeField] private GameObject CollectButton;
-    [SerializeField] private GameObject SellButton;
     
 
-    public bool isUIOpened { get; private set; }
+
     // Start is called before the first frame update
     private void Start()
     {
-        if(LakeInventoryUI==null) LakeInventoryUI = UIManager.Instance.transform.parent.Find("LakeInventoryUI").gameObject;
+        if(LakeInventoryCanvas==null) LakeInventoryCanvas = UIManager.Instance.transform.parent.GetComponentsInChildren<Transform>()
+                             .FirstOrDefault(c => c.gameObject.name == "LakeUICanvas")?.gameObject;
 
-        LakeInventoryUI.SetActive(false);
+        LakeInventoryCanvas.SetActive(false);
     }
 
     private IEnumerator UpdateUIStatus()
@@ -56,14 +54,14 @@ public class LakeUIController : GenericSingletonClass<LakeUIController>
 
         StartCoroutine(UpdateUIStatus());
         
-        LakeInventoryUI.SetActive(true);
+        LakeInventoryCanvas.SetActive(true);
     }
 
     public void CloseLakeUI()
     {
-        LakeInventoryUI.SetActive(false);
+        LakeInventoryCanvas.SetActive(false);
 
-        //ClearingUpButton();
+        ClearingUpButton();
 
         this.lakeInventory = null;
      
@@ -80,6 +78,8 @@ public class LakeUIController : GenericSingletonClass<LakeUIController>
             InventoryItemData soldItem = lakeInventory.RemoveItem();
             PlayerResourceManager.Instance.ChangeMoney(soldItem.itemSellPrice);
         }
+        
+        CloseLakeUI();
     }
     private void ButtonEventCollectItem()
     {
@@ -88,6 +88,8 @@ public class LakeUIController : GenericSingletonClass<LakeUIController>
             InventoryItemData collectedItem = lakeInventory.RemoveItem();
             QuestSystem.QuestManager.Instance.SendProgressFromQuestManagerToQuest(collectedItem);
         }
+
+        CloseLakeUI();
     }
 
     private void SettingUpButton()
@@ -115,19 +117,28 @@ public class LakeUIController : GenericSingletonClass<LakeUIController>
 
         FishItemData fish = lakeInventory.GetCurrentSavedItemData() as FishItemData;
         SellButton.transform.Find("Icon").Find("Text (TMP)").GetComponent<TextMeshProUGUI>().text = string.Format("Gold: +{0}", fish.itemSellPrice);
-        CollectButton.GetComponent<ButtonScript>().onClick.AddListener(() => ButtonEventCollectItem());
+        SellButton.GetComponent<ButtonScript>().onClick.AddListener(() => ButtonEventSellItem());
     }
 
     private void SettingUpUiInformation()
     {
         FishItemData fish = lakeInventory.GetCurrentSavedItemData() as FishItemData;
         CurrentFishImages.sprite = fish.icon;
+        CurrentFishName.text = fish.displayName;
     }
-    // private void ClearingUpButton()
-    // {
-    //     ClearingUpSellGrid();
-    //     ClearingUpCollectGrid();
-    // }
+    
+    private void ClearingUpButton()
+    {
+        // ClearingUpSellGrid();
+        // ClearingUpCollectGrid();
+        ClearListener();
+    }
+
+    private void ClearListener()
+    {
+        CollectButton.GetComponent<ButtonScript>().onClick.RemoveAllListeners();
+        SellButton.GetComponent<ButtonScript>().onClick.RemoveAllListeners();
+    }
 
     // private void ClearingUpSellGrid()
     // {
