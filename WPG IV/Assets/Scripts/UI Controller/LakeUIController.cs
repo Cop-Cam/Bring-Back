55a6d4a2dd20ff08065a4c1bbbeade3df7787ed9
PJacketG <anantamiyoruwijaya2@gmail.com>
2023-04-11 17:56:10 +0700

    Update UI For Lake
    - Add Not Workling UI for lake

--- WPG IV/Assets/Scripts/UI Controller/LakeUIController.cs
+++ WPG IV/Assets/Scripts/UI Controller/LakeUIController.cs
@@ -13,21 +13,28 @@ public class LakeUIController : GenericSingletonClass<LakeUIController>
     
     [SerializeField] private TextMeshProUGUI CurrentPlayerMoneyText;
 
+    [SerializeField] private Image CurrentFishImages;
 
-    [Header("Insert A Prefab for Collect Button and The Grid Which The Button Will Be Instantiated")]
-    [SerializeField] private GameObject CollectButtonPrefab;
-    [SerializeField] private GameObject CollectButtonGridLayout;
 
-    [Header("Insert A Prefab for Sell Button and The Grid Which The Button Will Be Instantiated")]
-    [SerializeField] private GameObject SellButtonPrefab;
-    [SerializeField] private GameObject SellButtonGridLayout;
+    // [Header("Insert A Prefab for Collect Button and The Grid Which The Button Will Be Instantiated")]
+    // [SerializeField] private GameObject CollectButtonPrefab;
+    // [SerializeField] private GameObject CollectButtonGridLayout;
+
+    // [Header("Insert A Prefab for Sell Button and The Grid Which The Button Will Be Instantiated")]
+    // [SerializeField] private GameObject SellButtonPrefab;
+    // [SerializeField] private GameObject SellButtonGridLayout;
+
+    [SerializeField] private GameObject CollectButton;
+    [SerializeField] private GameObject SellButton;
+    
 
     public bool isUIOpened { get; private set; }
     // Start is called before the first frame update
     private void Start()
     {
         if(LakeInventoryUI==null) LakeInventoryUI = UIManager.Instance.transform.parent.Find("LakeInventoryUI").gameObject;
-        
+
+        LakeInventoryUI.SetActive(false);
     }
 
     private IEnumerator UpdateUIStatus()
@@ -42,6 +49,8 @@ public class LakeUIController : GenericSingletonClass<LakeUIController>
         InputManager.Instance.IsPlayerAllowedToDoPlayerMapsInput(false);
 
         this.lakeInventory = lakeInventory;
+
+        SettingUpUiInformation();
         
         SettingUpButton();
 
@@ -54,7 +63,7 @@ public class LakeUIController : GenericSingletonClass<LakeUIController>
     {
         LakeInventoryUI.SetActive(false);
 
-        ClearingUpButton();
+        //ClearingUpButton();
 
         this.lakeInventory = null;
      
@@ -89,37 +98,49 @@ public class LakeUIController : GenericSingletonClass<LakeUIController>
 
     private void SettingUpCollectButton()
     {
-        
-        CollectButtonPrefab.transform.Find("Icon").GetComponent<Image>().sprite = lakeInventory.GetCurrentSavedItemData().icon;
-        CollectButtonPrefab.GetComponent<ButtonScript>().onClick.AddListener(() => ButtonEventCollectItem());
-        Instantiate(CollectButtonPrefab, CollectButtonGridLayout.transform);
+        // CollectButtonPrefab.transform.Find("Icon").GetComponent<Image>().sprite = lakeInventory.GetCurrentSavedItemData().icon;
+        // CollectButtonPrefab.GetComponent<ButtonScript>().onClick.AddListener(() => ButtonEventCollectItem());
+        // Instantiate(CollectButtonPrefab, CollectButtonGridLayout.transform);
+
+        FishItemData fish = lakeInventory.GetCurrentSavedItemData() as FishItemData;
+        CollectButton.transform.Find("Icon").Find("Text (TMP)").GetComponent<TextMeshProUGUI>().text = string.Format("{0}: +{1}", fish.fishTypes, fish.fishPoint);
+        CollectButton.GetComponent<ButtonScript>().onClick.AddListener(() => ButtonEventCollectItem());
     }
     private void SettingUpSellButton()
     {
-        SellButtonPrefab.transform.Find("Harga").transform.Find("HargaText").GetComponent<TextMeshProUGUI>().text = lakeInventory.GetCurrentSavedItemData().itemSellPrice.ToString();
-        SellButtonPrefab.transform.Find("Icon").GetComponent<Image>().sprite = lakeInventory.GetCurrentSavedItemData().icon;
-        SellButtonPrefab.GetComponent<ButtonScript>().onClick.AddListener(() => ButtonEventSellItem());
-        Instantiate(SellButtonPrefab, SellButtonGridLayout.transform);
+        // SellButtonPrefab.transform.Find("Harga").transform.Find("HargaText").GetComponent<TextMeshProUGUI>().text = lakeInventory.GetCurrentSavedItemData().itemSellPrice.ToString();
+        // SellButtonPrefab.transform.Find("Icon").GetComponent<Image>().sprite = lakeInventory.GetCurrentSavedItemData().icon;
+        // SellButtonPrefab.GetComponent<ButtonScript>().onClick.AddListener(() => ButtonEventSellItem());
+        // Instantiate(SellButtonPrefab, SellButtonGridLayout.transform);
+
+        FishItemData fish = lakeInventory.GetCurrentSavedItemData() as FishItemData;
+        SellButton.transform.Find("Icon").Find("Text (TMP)").GetComponent<TextMeshProUGUI>().text = string.Format("Gold: +{0}", fish.itemSellPrice);
+        CollectButton.GetComponent<ButtonScript>().onClick.AddListener(() => ButtonEventCollectItem());
     }
 
-    private void ClearingUpButton()
+    private void SettingUpUiInformation()
     {
-        ClearingUpSellGrid();
-        ClearingUpCollectGrid();
+        FishItemData fish = lakeInventory.GetCurrentSavedItemData() as FishItemData;
+        CurrentFishImages.sprite = fish.icon;
     }
+    // private void ClearingUpButton()
+    // {
+    //     ClearingUpSellGrid();
+    //     ClearingUpCollectGrid();
+    // }
 
-    private void ClearingUpSellGrid()
-    {
-        foreach(Transform SellButtonPrefab in SellButtonGridLayout.transform)
-        {
-            GameObject.Destroy(SellButtonPrefab.gameObject);
-        }
-    }
-    private void ClearingUpCollectGrid()
-    {
-        foreach(Transform CollectButtonPrefab in CollectButtonGridLayout.transform)
-        {
-            GameObject.Destroy(CollectButtonPrefab.gameObject);
-        }
-    }
+    // private void ClearingUpSellGrid()
+    // {
+    //     foreach(Transform SellButtonPrefab in SellButtonGridLayout.transform)
+    //     {
+    //         GameObject.Destroy(SellButtonPrefab.gameObject);
+    //     }
+    // }
+    // private void ClearingUpCollectGrid()
+    // {
+    //     foreach(Transform CollectButtonPrefab in CollectButtonGridLayout.transform)
+    //     {
+    //         GameObject.Destroy(CollectButtonPrefab.gameObject);
+    //     }
+    // }
 }
