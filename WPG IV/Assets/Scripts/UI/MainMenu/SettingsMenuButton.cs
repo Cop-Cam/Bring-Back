24bcf5aa167bb4b4122d2f1d1ae96a8414431b34
restudo <restudooctavriandi@gmail.com>
2023-05-20 20:43:08 +0700

    change in main menu UI

--- WPG IV/Assets/Scripts/UI/MainMenu/SettingsMenuButton.cs
+++ WPG IV/Assets/Scripts/UI/MainMenu/SettingsMenuButton.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class SettingsMenuButton : MonoBehaviour
+public class SettingsMenuButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] MenuButtonController menuButtonController;
+    [SerializeField] Animator animator;
+    [SerializeField] int thisIndex;
+
+    private bool click = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (menuButtonController.index == thisIndex)
+        {
+            animator.SetBool("selected", true);
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                animator.SetTrigger("rightPressed");
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                animator.SetTrigger("leftPressed");
+            }
+        }
+        else
+        {
+            animator.SetBool("selected", false);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        menuButtonController.index = thisIndex;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!click)
+            click = true;
     }
 }
